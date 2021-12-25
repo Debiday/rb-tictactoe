be6415a29fa2c336c2bc1d1371952efd126e2af8
Deborah Ong <deborah.oyt@gmail.com>
2021-12-25 00:58:51 -0800

    add any_empty and none_empty tests

--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Array do
-
   context '#all_empty?' do
     it 'returns true if all elements of the Array are empty' do
       expect(['', '', ''].all_empty?).to be true
@@ -16,28 +15,37 @@ describe Array do
 
   context '#all_same?' do
     it 'returns true if all elements of the Array are the same' do
-      expect(["A","A","A"].all_same?).to be true
+      expect(%w[A A A].all_same?).to be true
     end
     it 'returns false if some of the Array elements are not the same' do
-      expect(["A","B","A"].all_same?).to be false
+      expect(%w[A B A].all_same?).to be false
     end
     it 'returns true if the Array is empty' do
       expect([].all_same?).to be true
     end
-end
+  end
 
-    context '#any_empty?' do
-      it 'returns true if there are any elements of the Array that are empty' do
-        expect(["A","","A"].any_empty?).to be true
-      end
-      it 'returns false if none of the elements of the Array are empty' do
-      expect(["A","A","A"].any_empty?).to be false
-      end
-      it 'returns true if the Array is empty' do
-        expect([].any_empty?).to be false
-      end
-end
+  context '#any_empty?' do
+    it 'returns true if there are any elements of the Array that are empty' do
+      expect(['A', '', 'A'].any_empty?).to be true
+    end
+    it 'returns false if none of the elements of the Array are empty' do
+      expect(%w[A A A].any_empty?).to be false
+    end
+    it 'returns true if the Array is empty' do
+      expect([].any_empty?).to be false
+    end
+  end
 
-
-
+  context 'none_empty?' do
+    it 'returns true if none of the elements of the Array are empty' do
+      expect(%w[A A A].none_empty?).to be true
+    end
+    it 'returns false if some of the elements of the Array are empty' do
+      expect(['A', 'A', ''].none_empty?).to be false
+    end
+    it 'returns true if the Array is empty' do
+        expect([].none_empty?).to be true
+    end
+  end
 end
