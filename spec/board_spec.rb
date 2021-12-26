7d9ce233d023dde48638cd0d6e8bbba2f6165585
Deborah Ong <deborah.oyt@gmail.com>
2021-12-25 17:59:32 -0800

    complete board and tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,12 +6,10 @@ module TicTacToe
       it 'initializes the board with a grid' do
         expect { Board.new(grid: 'grid') }.to_not raise_error
       end
-
       it 'sets the grid with three rows by default' do
         board = Board.new
         expect(board.grid.size).to eq(3)
       end
-
       it 'creates three things in each row by default' do
         board = Board.new
         board.grid.each do |row|
@@ -19,12 +17,14 @@ module TicTacToe
         end
       end
     end
+
     context '#grid' do
       it 'returns the grid' do
         board = Board.new(grid: 'blah')
         expect(board.grid).to eq 'blah'
       end
     end
+
     context '#get_cell' do
       it 'returns the cell based on the (x, y) coordinate' do
         grid = [['', '', ''], ['', '', 'something'], ['', '', '']]
@@ -32,6 +32,7 @@ module TicTacToe
         expect(board.get_cell(2, 1)).to eq 'something'
       end
     end
+    
     # struct, to create an object instead of using cell objects
     # reduce needless dependency in test suite
     context '#set_cell' do
@@ -44,26 +45,76 @@ module TicTacToe
       end
     end
 
-    #stub is a special type of method that "stands in" for an existing method
-    #used for methods that doesn't exist yet
+    TestCell = Struct.new(:value)
+    let(:x_cell) { TestCell.new('X') }
+    let(:y_cell) { TestCell.new('Y') }
+    let(:empty) { TestCell.new }
+
+    # stub is a special type of method that "stands in" for an existing method
+    # used for methods that doesn't exist yet
     context '#game_over' do
       it 'returns :winner if winner? is true' do
         board = Board.new
         board.stub(:winner?) { true }
         expect(board.game_over).to eq :winner
       end
-
       it 'returns :draw if winner? is false and draw? is true' do
         board = Board.new
         board.stub(:winner?) { false }
         board.stub(:draw?) { true }
         expect(board.game_over).to eq :draw
       end
-
       it 'returns false if winner? is false and draw? is false' do
         board = Board.new
         board.stub(:winner?) { false }
         board.stub(:draw?) { false }
+        expect(board.game_over).to be false
+      end
+
+      # updated and without stubbing
+      it 'returns  :winner when row has objects with values that are all the same' do
+        grid = [
+          [x_cell, x_cell, x_cell],
+          [y_cell, x_cell, y_cell],
+          [y_cell, y_cell, empty]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to eq :winner
+      end
+      it 'returns :winner when column has objects with values that are all the same' do
+        grid = [
+          [y_cell, x_cell, empty],
+          [y_cell, x_cell, y_cell],
+          [y_cell, x_cell, empty]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to eq :winner
+      end
+      it 'returns :winner when diagonal has objects with values that are all the same' do
+        grid = [
+          [x_cell, empty, empty],
+          [y_cell, x_cell, y_cell],
+          [y_cell, x_cell, x_cell]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to eq :winner
+      end
+      it 'returns :draw when all the spaces on the board are taken' do
+        grid = [
+          [x_cell, y_cell, x_cell],
+          [y_cell, x_cell, y_cell],
+          [y_cell, x_cell, y_cell]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to eq :draw
+      end
+      it 'returns false when there is no winner or draw' do
+        grid = [
+          [x_cell, empty, empty],
+          [y_cell, empty, empty],
+          [y_cell, empty, empty]
+        ]
+        board = Board.new(grid: grid)
         expect(board.game_over).to be false
       end
     end
